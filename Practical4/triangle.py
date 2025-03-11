31564c800c362e9b8744a06d8821e307f030dedb
Johnsnow <shy944664@gmail.com>
2025-03-11 11:48:20 +0800

    add some comment

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,7 +1,7 @@
 n=int(input("the length of triangle seq:"))
 sum=0
-for i in range(1,n+1) :
-	for j in range(i):
+for i in range(1,n+1) :		
+	for j in range(i):		#each loop calculate the sum of the certain triangle
 		sum += 	j+1
 	print("the sum is:",sum)
-	sum=010
+	sum=0
