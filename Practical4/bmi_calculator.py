44464a05f8de9ee189dafb8e92b119de75d7be04
Johnsnow <shy944664@gmail.com>
2025-03-12 14:40:00 +0800

    change the method of commenting

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -7,20 +7,19 @@ elif 18.5 <= bmi <= 30:
     print("your BMI is:", bmi, "you are normal")
 else:
     print("your BMI is:", bmi, "you are too thin")
-'''
-pseudocode:
-BEGIN
-    PROMPT user to input their body weight (in kg) and STORE it in variable weight
-    PROMPT user to input their height (in m) and STORE it in variable height
-
-    CALCULATE bmi as weight divided by the square of height
-
-    IF bmi is greater than 30 THEN
-        PRINT "your BMI is:" followed by the value of bmi and "you are obese"
-    ELSE IF bmi is between 18.5 and 30 (inclusive) THEN
-        PRINT "your BMI is:" followed by the value of bmi and "you are normal"
-    ELSE
-        PRINT "your BMI is:" followed by the value of bmi and "you are too thin"
-    END IF
-END
-'''
+    
+#pseudocode:
+#BEGIN
+#    PROMPT user to input their body weight (in kg) and STORE it in variable weight
+#    PROMPT user to input their height (in m) and STORE it in variable height
+#
+#    CALCULATE bmi as weight divided by the square of height
+#
+#   IF bmi is greater than 30 THEN
+#        PRINT "your BMI is:" followed by the value of bmi and "you are obese"
+#    ELSE IF bmi is between 18.5 and 30 (inclusive) THEN
+#        PRINT "your BMI is:" followed by the value of bmi and "you are normal"
+#    ELSE
+#        PRINT "your BMI is:" followed by the value of bmi and "you are too thin"
+#    END IF
+#END
--- Practical4/variables.py
+++ Practical4/variables.py
@@ -8,22 +8,21 @@ if c > f :
 	print("c > f")
 else:
 	print("f > c")
-'''shwenyu@hanyudeMacBook-Pro IBI1_2024-25 %  /usr/bin/env /usr/bin/python3 /Users/shwenyu/.vscode/extensions/ms-python.debugpy-2025
-.0.1-darwin-arm64/bundled/libs/debugpy/adapter/../../debugpy/launcher 56729 -- /Users/shwenyu/Documents/GitHub/IBI1_2024-25/Pract
-ical4/variables.py 
-f > c
-this means that 
-'''
+#shwenyu@hanyudeMacBook-Pro IBI1_2024-25 %  /usr/bin/env /usr/bin/python3 /Users/shwenyu/.vscode/extensions/ms-python.debugpy-2025
+#.0.1-darwin-arm64/bundled/libs/debugpy/adapter/../../debugpy/launcher 56729 -- /Users/shwenyu/Documents/GitHub/IBI1_2024-25/Pract
+#ical4/variables.py 
+#f > c
+#this means that taking buses is faster than driving cars 
+
 
 for X in (True, False):
 	for Y in (True, False):
 		W= X and Y
 		print(X,"and",Y,"=",W)    
        
-'''hwenyu@hanyudeMacBook-Pro IBI1_2024-25 % /usr/bin/python3 /Users/shwenyu/Documents/GitHub/IBI1_2024-25/Practica
-l4/variables.py
-f > c
-True and True = True
-True and False = False
-False and True = False
-False and False = False'''
+#hwenyu@hanyudeMacBook-Pro IBI1_2024-25 % /usr/bin/python3 /Users/shwenyu/Documents/GitHub/IBI1_2024-25/Practica
+#l4/variables.py
+#f > c
+#True and False = False
+#False and True = False
+#False and False = False'''
