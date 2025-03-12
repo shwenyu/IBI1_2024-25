14026c89cb50f6b5eb4fd8ec0c7a7c8d7d16b4fb
Johnsnow <shy944664@gmail.com>
2025-03-12 14:00:52 +0800

    further comment for the cides

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -11,7 +11,9 @@ else:
 '''shwenyu@hanyudeMacBook-Pro IBI1_2024-25 %  /usr/bin/env /usr/bin/python3 /Users/shwenyu/.vscode/extensions/ms-python.debugpy-2025
 .0.1-darwin-arm64/bundled/libs/debugpy/adapter/../../debugpy/launcher 56729 -- /Users/shwenyu/Documents/GitHub/IBI1_2024-25/Pract
 ical4/variables.py 
-f > c'''
+f > c
+this means that 
+'''
 
 for X in (True, False):
 	for Y in (True, False):
